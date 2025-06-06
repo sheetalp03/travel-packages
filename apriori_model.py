19a4c378b1e568303fd691de24235358d5efc22f
Sheetal Pawar <sheetalspawar1804@gmail.com>
2025-06-07 01:09:01 +0530

    Update apriori_model.py

--- apriori_model.py
+++ apriori_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 
-def generate_rules(file_path='C:/Users/shrip/OneDrive/Desktop/travel-packages-master/data/travel_dataset.csv'):
+def generate_rules(file_path='data/travel_dataset.csv'):
     df = pd.read_csv(file_path)
     df_bool = df[['Flight', 'Hotel', 'Local_Tour', 'Car_Rental', 'Insurance']] == 'Yes'
 
